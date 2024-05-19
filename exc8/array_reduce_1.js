33b1903a4ae4e033183cb0dcc1e652451daf86bf
Paz Gueta <pazgueta@gmail.com>
2024-05-19 11:28:05 +0300

    ex9 upload

--- exc8/array_reduce_1.js
+++ exc8/array_reduce_1.js
@@ -1,7 +1,19 @@
 function return_sum (numbers) {
-    return numbers.reduce((sum, element) => sum += element);
+    return numbers.reduce((sum, element) => sum + element);
 }
 
 
 let numbers = [7, -2, 5, 3, 1, 4];
 console.log(return_sum(numbers)); 
+
+
+
+function return_even_array (numbers) {
+    const initialValue = 0;
+    return numbers.reduce((count, element) => (element % 2 == 0 ? count +1 : count), initialValue);
+}
+
+// in reduce you need an initial value so it won't take the first value of the array
+
+console.log(return_even_array(numbers)); 
+
